4b41a2f827de6bd25c88d9dc199dcd39b2d15d64
sbutler01 <61069348+sbutler01@users.noreply.github.com>
2020-04-15 09:35:07 -0600

    Add files via upload

---
+++ interest_calculator.js
@@ -0,0 +1,24 @@
+function calcInterest(){
+	var initialMoney = document.querySelector('#initial_value').value;
+	var interestRate = 0.01*document.querySelector('#operator_value').value;
+	var loanTime = parseInt(document.querySelector('#operator_length').value);
+
+	var operatorType = document.querySelector('#operator_type').value;
+	var interestLength = parseInt(document.querySelector('#interest_length').value);
+
+	var timeCompounds;
+
+	if (operatorType === 'Simple') {
+		timeCompounds = loanTime;
+	} else {
+		timeCompounds = parseInt(operatorType)
+	}
+	var numberTimes = loanTime/interestLength
+
+	var numberCompounds = (interestLength - (interestLength%timeCompounds))/timeCompounds;
+
+	var moneyEarned = initialMoney*(1+(interestRate/numberCompounds))**(numberCompounds*numberTimes);
+
+	document.querySelector("#result").innerHTML = Math.round(moneyEarned);
+
+}
